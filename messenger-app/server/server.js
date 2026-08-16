90bd6f79525823135521ac48c34ac08a3206faa7
KOUSHIKGOLLAPALLI <koushikgollapalli3@gmail.com>
2026-08-16 10:18:20 +0530

    Update server.js

--- messenger-app/server/server.js
+++ messenger-app/server/server.js
@@ -49,7 +49,6 @@ app.use(errorHandler);
 registerSocketHandlers(io);
 
 const PORT = process.env.PORT || 5000;
-
-server.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
